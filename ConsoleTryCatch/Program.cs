e3a2ba4a7b99fffc4737a9d5d4cb08ce9106b514
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-14 14:32:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleTryCatch/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleTryCatch {
+
+    class Program {
+        static void Main()
+        {
+            int valor1 = 0;
+            int valor2 = 0;
+            int result = 0;
+            try
+            {
+                result = valor1 / valor2;                
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu o seguinte erro: {e.Message} \n linha do erro: {e.StackTrace}"); 
+            }
+
+        }
+    }
+}
