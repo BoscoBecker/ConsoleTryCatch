6f07a6b8c72b9b40c51e87581d40312524af790f
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-14 14:43:36 -0300

    Add upcasting by exceptio.

--- ConsoleTryCatch/Program.cs
+++ ConsoleTryCatch/Program.cs
@@ -1,20 +1,25 @@
-﻿namespace ConsoleTryCatch {
+﻿using System.Globalization;
+
+namespace ConsoleTryCatch {
 
     class Program {
         static void Main()
         {
-            int valor1 = 0;
-            int valor2 = 0;
+            int valor1 = int.Parse(Console.ReadLine());
+            int valor2 = int.Parse(Console.ReadLine()); 
             int result = 0;
             try
-            {
-                result = valor1 / valor2;                
+            { 
+                result = valor1 / valor2;              
 
             }
-            catch (Exception e)
+            catch (DivideByZeroException e) // using Upcasting by Exception class and call DivideByZeroException
             {
                 Console.WriteLine($"Ocorreu o seguinte erro: {e.Message} \n linha do erro: {e.StackTrace}"); 
             }
+
+            Console.WriteLine("Valor da divisão:" + result.ToString("F2", CultureInfo.InvariantCulture));
+
 
         }
     }
